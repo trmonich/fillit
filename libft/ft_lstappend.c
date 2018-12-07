041d36b51c6d685a10114afc7b9112932b48ce78
Alexandre STEPHAN <alesteph@student.42.fr>
2018-12-07 11:55:51 +0100

    ft_lstappend in libft

--- libft/ft_lstappend.c
+++ libft/ft_lstappend.c
@@ -6,7 +6,7 @@
 /*   By: trmonich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/29 11:29:53 by trmonich          #+#    #+#             */
-/*   Updated: 2018/11/29 14:23:09 by trmonich         ###   ########.fr       */
+/*   Updated: 2018/12/07 11:50:25 by alesteph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,10 @@ void	ft_lstappend(t_list **alst, t_list *new)
 	t_list	*list;
 
 	if (*alst == NULL)
+	{
 		ft_lstadd(alst, new);
+		return ;
+	}
 	list = *alst;
 	while (list->next)
 		list = list->next;
