974ee78efda7fe0bd97ee5aa95e9cb500e48dbff
Alexandre STEPHAN <alesteph@student.42.fr>
2018-12-07 11:19:21 +0100

    swaping branch

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: trmonich <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 14:08:42 by trmonich          #+#    #+#             */
-/*   Updated: 2018/12/07 08:44:25 by trmonich         ###   ########.fr       */
+/*   Updated: 2018/12/07 11:17:53 by alesteph         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <stdlib.h>
 # include <string.h>
 
-# define BUFF_SIZE 15
+# define BUFF_SIZE 20
 
 
 typedef struct		s_list
